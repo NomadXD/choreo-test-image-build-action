ecbb7df23e9b81655c4eb7de0e73b075471a81b2
Lahiru De Silva <lahirude@wso2.com>
2023-08-28 21:33:15 +0530

    inline dockerfile dist changes
    Signed-off-by: Lahiru De Silva <lahirude@wso2.com>

--- dist/index.js
+++ dist/index.js
@@ -13955,10 +13955,32 @@ try {
   //     shell: true,
   //   }
   // );
+  const dockerfileContent = `
+    # Use the official Newman base image
+    FROM postman/newman:alpine
+    
+    # Set a working directory in the container
+    WORKDIR /app
+    
+    # Build argument to pass the Postman collection path
+    ARG POSTMAN_COLLECTION
+    
+    # Copy the Postman collection to the container
+    COPY $POSTMAN_COLLECTION /app/collection.json
+    
+    # Define the command to run the collection using Newman
+    CMD ["run", "collection.json"]
+  `;
+
+  // Create a temporary Dockerfile
+  fs.writeFileSync("Dockerfile.inline", dockerfileContent);
+
   const dockerBuildProcess = spawn("docker", [
     "build",
     "-t",
     newImageTag,
+    "-f",
+    "Dockerfile.inline",
     "--build-arg",
     `POSTMAN_COLLECTION=${postmanCollectionPath}`,
     ".",
